f0205b7afcf5e873b923bc137526f0ef19370071
yavuzsonmez <contact@yavuzsonmez.com>
2021-07-19 12:12:28 +0200

    gnl

---
+++ get_next_line.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ysonmez <ysonmez@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/06/25 12:31:58 by ysonmez           #+#    #+#             */
+/*   Updated: 2021/07/19 12:09:26 by ysonmez          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+#include <stdio.h>
+#include <fcntl.h>
+
+char	*get_next_line(int fd)
+{
+	static char c;
+
+	
+}
+
+
+int main(void)
+{
+	int fd = open("text.txt", O_RDONLY);
+	if (fd == -1)
+		return (printf("ERROR\n"));
+	printf("%s\n", get_next_line(fd));
+	return (0);
+}
---
+++ get_next_line.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ysonmez <ysonmez@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/06/25 12:24:00 by ysonmez           #+#    #+#             */
+/*   Updated: 2021/07/19 12:02:19 by ysonmez          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <unistd.h>
+
+char	*get_next_line(int fd);
+
+#endif
