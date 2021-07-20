aab50a3650381771a03eec49716ff774a8dd8457
yavuzsonmez <contact@yavuzsonmez.com>
2021-07-20 14:29:43 +0200

    gnl

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: ysonmez <ysonmez@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/25 12:31:58 by ysonmez           #+#    #+#             */
-/*   Updated: 2021/07/20 11:59:13 by ysonmez          ###   ########.fr       */
+/*   Updated: 2021/07/20 14:28:42 by ysonmez          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,34 +20,32 @@ char	*get_next_line(int fd)
 		return (NULL);
 	static size_t i;
 	size_t k;
-	char *rd;
+	char *str;
+	char *catstr;
 	char *newline;
-
-	i = 0;
+	char c;
 	k = 0;
-	//if (!str)
-	//	str = (char *)malloc(sizeof(char));
-	//if (str[i] && str[i] == '\n')
-	//	i++;
-	//else
-	//{
-	//	free(str);
-	//	str = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
-	//	read(fd, str, BUFFER_SIZE);
-	//	str[BUFFER_SIZE] = '\0';
-	//}
 
-	rd = (char *)malloc(sizeof(char) * (BUFFER_SIZE + i + 1));
-	read(fd, rd, BUFFER_SIZE + i);
-	rd[BUFFER_SIZE] = '\0';
-	//if (str[i] != '\n')
-	//if (read(fd, rd, BUFFER_SIZE) == 0)
-	//	return (NULL);
-	//rd[BUFFER_SIZE] = '\0';
-	while(rd[k] && rd[k] != '\n')
-		k++;
-	newline = ft_substr(rd, i, k);
-	free(rd);
+	str = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	if (read(fd, str, BUFFER_SIZE) == 0)
+		return (NULL);
+	str[BUFFER_SIZE] = '\0';
+	while(str[i] && str[i] != '\n')
+	{
+		i++;
+		if (str[i] == '\n')
+			break;
+		else if (str[i] == '\0')
+		{
+			if (read(fd, &c, 1) == 0)
+				return (NULL);
+			catstr = ft_strjoin(str, &c);
+			free(str);
+		}
+
+	}
+	newline = ft_substr(str, 0, i);
+	free(str);
 	return(newline);
 }
 
@@ -56,7 +54,10 @@ int main(void)
 	int fd = open("text.txt", O_RDONLY);
 	char *newline = get_next_line(fd);
 	printf("%s\n", newline);
-	printf("%s\n", get_next_line(fd));
+	//newline = get_next_line(fd);
+	//printf("%s\n", newline);
+	//newline = get_next_line(fd);
+	//printf("%s\n", newline);
 	//while (newline)
 	//{
 	//	printf("%s\n", newline);
