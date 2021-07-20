6a6ba6c1ab9d2760a9a14534c3e88795f8ff6c18
yavuzsonmez <contact@yavuzsonmez.com>
2021-07-20 09:10:26 +0200

    gnl

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: ysonmez <ysonmez@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/25 12:31:58 by ysonmez           #+#    #+#             */
-/*   Updated: 2021/07/19 18:31:15 by ysonmez          ###   ########.fr       */
+/*   Updated: 2021/07/20 09:09:46 by ysonmez          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,26 +16,27 @@
 
 char	*get_next_line(int fd)
 {
-	//static char c;
 	if (fd < 0)
 		return (NULL);
-	static char c;
-	char *str;
+	static char *str;
 	size_t		i;
 	char *newline;
-	//char *c;
 
-	//c = (char *)malloc(1);
-	str = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
 	i = 0;
-	read(fd, str, BUFFER_SIZE);
-	str[BUFFER_SIZE] = '\0';
+	if (!str)
+		str = (char *)malloc(sizeof(char));
+	if (str[i] && str[i] == '\n')
+		i++;
+	else
+	{
+		free(str);
+		str = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
+		read(fd, str, BUFFER_SIZE);
+		str[BUFFER_SIZE] = '\0';
+	}
 	while(str[i] && str[i] != '\n')
 		i++;
-	c = str[i];
 	newline = ft_substr(str, 0, i);
-	//ft_strjoin(str, c);
-	if (c == '\n')
 	return(newline);
 }
 
@@ -43,12 +44,12 @@ int main(void)
 {
 	int fd = open("text.txt", O_RDONLY);
 	char *newline = get_next_line(fd);
-	printf("%s\n", newline);
-	//while (newline)
-	//{
-	//	printf("%s\n", newline);
-	//	newline = get_next_line(fd);
-	//}
+	while (newline)
+	{
+		printf("%s\n", newline);
+		newline = get_next_line(fd);
+	}
 	close (fd);
+	fscanf(stdin, "c");
 	return (0);
 }
