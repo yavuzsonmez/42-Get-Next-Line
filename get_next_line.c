428ae83c8c2412f65e9fa673992e50fee50c31e5
yavuzsonmez <contact@yavuzsonmez.com>
2021-07-21 18:30:22 +0200

    gnl

--- get_next_line.c
+++ get_next_line.c
@@ -6,36 +6,44 @@
 /*   By: ysonmez <ysonmez@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/25 12:31:58 by ysonmez           #+#    #+#             */
-/*   Updated: 2021/07/21 17:20:41 by ysonmez          ###   ########.fr       */
+/*   Updated: 2021/07/21 18:28:49 by ysonmez          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 #include <stdio.h>
 #include <fcntl.h>
+#include <limits.h>
 
 char	*get_next_line(int fd)
 {
 	size_t i;
-	static char *str;
+	static char **arr;
 	char *newline;
 	char c;
 
 	i = 0;
 	if (fd == - 1)
 		return (NULL);
-	if (!str)
+	if (!arr)
 	{
-		str = (char *)ft_calloc(sizeof(char), (BUFFER_SIZE + 1));
-		if (!str || !read(fd, str, BUFFER_SIZE))
+		arr = (char **)malloc(sizeof(char *) * (10240 + 1));
+		if (!arr)
+			return (NULL);
+		arr[10240] = NULL;
+	}
+	if (!arr[fd])
+	{
+		arr[fd] = (char *)ft_calloc(sizeof(char), BUFFER_SIZE + 1);
+		if (!read(fd, arr[fd], BUFFER_SIZE))
 			return (NULL);
 	}
-	while(str[i] && str[i] != '\n')
+	while(arr[fd][i] && arr[fd][i] != '\n')
 		i++;
-	if (str[i] == '\n')
+	if (arr[fd][i] == '\n')
 	{
-		newline = ft_substr(str, 0, i);
-		str = ft_substr(str, i, BUFFER_SIZE - i - 1);
+		newline = ft_substr(arr[fd], 0, i);
+		arr[fd] = ft_substr(arr[fd], i, BUFFER_SIZE - i - 1);
 	}
 	else
 	{
@@ -43,17 +51,18 @@ char	*get_next_line(int fd)
 		{
 			if (!read(fd, &c, 1))
 				return (NULL);
-			newline = ft_strjoin(str, &c);
+			newline = ft_strjoin(arr[fd], &c);
 		}
 	}
-	if (str[i] == '\0')
-		free(str);
+	if (arr[fd][i] == '\0')
+		free(arr[fd]);
 	return(newline);
 }
 
 int main(void)
 {
 
+	printf("OPEN MAX %d\n", OPEN_MAX);
 	int fd1 = open("fd1.txt", O_RDONLY);
 	int fd2 = open("fd2.txt", O_RDONLY);
 	int fd3 = open("fd3.txt", O_RDONLY);
@@ -63,18 +72,16 @@ int main(void)
 	printf("%s\n", newline);
 	newline = get_next_line(fd3);
 	printf("%s\n", newline);
-	newline = get_next_line(fd1);
-	printf("%s\n", newline);
-	//newline = get_next_line(fd);
-	//printf("%s\n", newline);
-	//newline = get_next_line(fd);
+	//newline = get_next_line(fd1);
 	//printf("%s\n", newline);
 	//while (newline)
 	//{
 	//	printf("%s\n", newline);
 	//	newline = get_next_line(fd);
 	//}
-	close (fd);
+	close (fd1);
+	close (fd2);
+	close (fd3);
 	fscanf(stdin, "c");
 	return (0);
 }
