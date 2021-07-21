81d24d2ec5b1d4ff5c9b373553211c2d20edffbf
yavuzsonmez <contact@yavuzsonmez.com>
2021-07-21 17:21:19 +0200

    gnl

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: ysonmez <ysonmez@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/25 12:31:58 by ysonmez           #+#    #+#             */
-/*   Updated: 2021/07/21 15:10:46 by ysonmez          ###   ########.fr       */
+/*   Updated: 2021/07/21 17:20:41 by ysonmez          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,49 +16,57 @@
 
 char	*get_next_line(int fd)
 {
-	if (fd < 0)
-		return (NULL);
 	size_t i;
-	size_t k;
 	static char *str;
 	char *newline;
 	char c;
-	i = 0;
-	k = 0;
 
+	i = 0;
+	if (fd == - 1)
+		return (NULL);
 	if (!str)
 	{
 		str = (char *)ft_calloc(sizeof(char), (BUFFER_SIZE + 1));
-		if (!str || read(fd, str, BUFFER_SIZE) == 0)
+		if (!str || !read(fd, str, BUFFER_SIZE))
 			return (NULL);
 	}
 	while(str[i] && str[i] != '\n')
-	{
 		i++;
-		if (str[i] == '\n')
-			break ;
-		else if (str[i] == '\0')
+	if (str[i] == '\n')
+	{
+		newline = ft_substr(str, 0, i);
+		str = ft_substr(str, i, BUFFER_SIZE - i - 1);
+	}
+	else
+	{
+		while (c != '\n')
 		{
-			if (read(fd, &c, 1) == 0)
+			if (!read(fd, &c, 1))
 				return (NULL);
-			str = ft_strjoin(str, &c);
+			newline = ft_strjoin(str, &c);
 		}
 	}
-	newline = ft_substr(str, 0, i);
-	if (i < BUFFER_SIZE)
-		str = ft_substr(str, i, BUFFER_SIZE);
-	if (i == BUFFER_SIZE)
+	if (str[i] == '\0')
 		free(str);
 	return(newline);
 }
 
 int main(void)
 {
-	int fd = open("text.txt", O_RDONLY);
-	char *newline = get_next_line(fd);
+
+	int fd1 = open("fd1.txt", O_RDONLY);
+	int fd2 = open("fd2.txt", O_RDONLY);
+	int fd3 = open("fd3.txt", O_RDONLY);
+	char *newline = get_next_line(fd1);
 	printf("%s\n", newline);
-	newline = get_next_line(fd);
+	newline = get_next_line(fd2);
 	printf("%s\n", newline);
+	newline = get_next_line(fd3);
+	printf("%s\n", newline);
+	newline = get_next_line(fd1);
+	printf("%s\n", newline);
+	//newline = get_next_line(fd);
+	//printf("%s\n", newline);
 	//newline = get_next_line(fd);
 	//printf("%s\n", newline);
 	//while (newline)
