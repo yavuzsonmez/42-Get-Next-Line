6b19aeaee46d0daf7f3b05b38ef552b9cb9b8e44
yavuzsonmez <contact@yavuzsonmez.com>
2021-07-22 11:17:25 +0200

    gnl

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: ysonmez <ysonmez@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/25 12:31:58 by ysonmez           #+#    #+#             */
-/*   Updated: 2021/07/21 19:31:49 by ysonmez          ###   ########.fr       */
+/*   Updated: 2021/07/22 11:15:26 by ysonmez          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,28 +18,31 @@
 char	*get_next_line(int fd)
 {
 	size_t i;
+	int	rbytes;
 	static char **arr;
 	char *newline;
-	char c;
+	char *c;
 
 	i = 0;
 	if (fd == -1 || BUFFER_SIZE < 1)
 		return (NULL);
 	if (!arr)
 	{
-		arr = (char **)malloc(sizeof(char *) * (10240 + 1));
+		arr = (char **)malloc(sizeof(char *) * (4096 + 1));
 		if (!arr)
 			return (NULL);
-		arr[10240] = NULL;
+		arr[4096] = NULL;
 	}
 	if (!arr[fd])
 	{
 		arr[fd] = (char *)ft_calloc(sizeof(char), BUFFER_SIZE + 1);
-		if (!read(fd, arr[fd], BUFFER_SIZE))
+		rbytes = read(fd, arr[fd], BUFFER_SIZE);
+		if (rbytes < 0)
 			return (NULL);
 	}
-	while(arr[fd][i] && arr[fd][i] != '\n')
+	while (arr[fd][i])
 		i++;
+	i--;
 	if (arr[fd][i] == '\n')
 	{
 		newline = ft_substr(arr[fd], 0, ++i);
@@ -47,30 +50,33 @@ char	*get_next_line(int fd)
 	}
 	else
 	{
-		c = 'q';
-		while (c != '\n')
+		c = (char *)ft_calloc(sizeof(char), 2);
+		while (*c != '\n')
 		{
-			if (!read(fd, &c, 1))
+			rbytes = read(fd, c, 1);
+			if (rbytes < 0)
 				return (NULL);
-			newline = ft_strjoin(arr[fd], &c);
+			else if (rbytes == 0)
+				break ;
+			newline = ft_strjoin(arr[fd], c);
+			//free(c);
 		}
 	}
-	if (arr[fd][i] == '\0')
+	if (rbytes == 0)
 		free(arr[fd]);
 	return(newline);
 }
 
 int main(void)
 {
-
 	//printf("OPEN MAX %d\n", OPEN_MAX);
 	int fd1 = open("fd1.txt", O_RDONLY);
 	int fd2 = open("fd2.txt", O_RDONLY);
 	int fd3 = open("fd3.txt", O_RDONLY);
 	char *newline = get_next_line(fd1);
 	printf("%s", newline);
-	newline = get_next_line(fd2);
-	printf("%s", newline);
+	//newline = get_next_line(fd2);
+	//printf("%s", newline);
 	//newline = get_next_line(fd3);
 	//printf("%s\n", newline);
 	//newline = get_next_line(fd1);
